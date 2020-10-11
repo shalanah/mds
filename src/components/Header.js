091cf516423dc5332e27833e693369f9ee2e4401
shalanah <shalanahfaith@gmail.com>
2020-10-11 01:42:03 -0500

    Banner update

--- src/components/Header.js
+++ src/components/Header.js
@@ -325,7 +325,7 @@ export default ({}) => {
         </span>
         <span style={{ textAlign: 'left' }}>
           We are unable to book lessons at this time. We are sorry for any
-          inconvenience. Please check back after October 15th.
+          inconvenience. Please check back after November 1st.
         </span>
       </COVIDBanner>
       {/* Mobile */}
